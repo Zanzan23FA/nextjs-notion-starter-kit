5e1ff7ceb33ee553ab597b9b0c232c277a7cacf5
Zanzan23FA <164753039+Zanzan23FA@users.noreply.github.com>
2024-03-24 18:07:33 +0100

    ZANUpdate site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'https://www.notion.so/About-Me-2b064ff4193a4c53b75b3531ad671f45?pvs=4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'ZAN',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'zan',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
